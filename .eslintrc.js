61d5e97ab5beb8159c63082e5b677a2ab1d40bce
April <April@Rain>
2020-01-18 18:28:01 +0000

    use airbnb lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,91 +1,33 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
+  env: {
+    browser: true,
+    es6: true,
   },
-  "extends": [
-    "eslint:recommended",
+  extends: [
     "plugin:react/recommended",
+    "airbnb",
   ],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    sourceType: "module",
+    ecmaVersion: 2018,
   },
-  "plugins": [
+  plugins: [
     "react",
   ],
-  "rules": {
-    "indent": [
+  rules: {
+    "quotes": [
       "error",
-      2,
+      "double",
     ],
     "linebreak-style": [
       "error",
       "windows",
     ],
-    "quotes": [
-      "error",
-      "double",
-    ],
-    "semi": [
-      "error",
-      "always",
-    ],
-    "comma-spacing": ["error", { "before": false, "after": true }],
-    "comma-dangle": [
-      "error",
-      "always-multiline",
-    ],
-    "comma-style": [
-      "error",
-      "last",
-    ],
-    "no-undef": "warn",
-    "no-unused-vars": "warn",
-    "brace-style": [
-      "error",
-      "1tbs",
-    ],
-    "block-spacing": "error",
-    "array-bracket-spacing": [
-      "error",
-      "never",
-    ],
-    "implicit-arrow-linebreak": [
-      "error",
-      "beside",
-    ],
-    "arrow-body-style": [
-      "error",
-      "as-needed",
-    ],
-    "no-trailing-spaces": "error",
-    "no-multi-spaces": "warn",
-    "no-whitespace-before-property": "error",
-    "space-before-blocks": "error",
-    "keyword-spacing": ["error", { "before": true }],
-    "space-in-parens": ["error", "never"],
-    "space-infix-ops": "error",
-    "key-spacing": ["error", { "beforeColon": false }],
-    "computed-property-spacing": ["error", "never"],
-    "spaced-comment": [
-      "error",
-      "always",
-      {
-        "exceptions": ["*"],
-      },
-    ],
-    "object-curly-spacing": ["error", "always"],
-    "semi-spacing": "error",
-    "no-case-declarations": "warn",
-    "no-empty-pattern": "warn",
-    "no-floating-decimal": "error",
   },
 };
