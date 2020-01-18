90119b799cbd32fb4452176e44ce208c144d7b6f
April <April@Rain>
2020-01-18 17:36:51 +0000

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,11 +39,53 @@ module.exports = {
       "always",
     ],
     "comma-spacing": ["error", { "before": false, "after": true }],
-    "no-undef": "warn",
-    "no-unused-vars": "warn",
     "comma-dangle": [
       "error",
       "always-multiline",
-    ]
-  }
+    ],
+    "comma-style": [
+      "error",
+      "last",
+    ],
+    "no-undef": "warn",
+    "no-unused-vars": "warn",
+    "brace-style": [
+      "error",
+      "1tbs",
+    ],
+    "block-spacing": "error",
+    "array-bracket-spacing": [
+      "error",
+      "never",
+    ],
+    "implicit-arrow-linebreak": [
+      "error",
+      "beside",
+    ],
+    "arrow-body-style": [
+      "error",
+      "as-needed",
+    ],
+    "no-trailing-spaces": "error",
+    "no-multi-spaces": "warn",
+    "no-whitespace-before-property": "error",
+    "space-before-blocks": "error",
+    "keyword-spacing": ["error", { "before": true }],
+    "space-in-parens": ["error", "never"],
+    "space-infix-ops": "error",
+    "key-spacing": ["error", { "beforeColon": false }],
+    "computed-property-spacing": ["error", "never"],
+    "spaced-comment": [
+      "error",
+      "always",
+      {
+        "exceptions": ["*"],
+      },
+    ],
+    "object-curly-spacing": ["error", "always"],
+    "semi-spacing": "error",
+    "no-case-declarations": "warn",
+    "no-empty-pattern": "warn",
+    "no-floating-decimal": "error",
+  },
 };
