562c0d480c22887fcea07dacd8deeccbb95b41c1
April <April@Rain>
2020-01-18 10:31:35 +0000

    eslint

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function Root() {
-    return <h1>Hello, world.</h1>;
+  return <h1>Hello, world.</h1>;
 }
 
 // Render the Root element into the DOM
 ReactDOM.render(
-    <Root />,
-    document.getElementById("root"),
+  <Root />,
+  document.getElementById("root"),
 );
