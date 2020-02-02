1d1e6687a8b0963d327f2963f17b76617a0941ee
April <April@Rain>
2020-02-02 14:48:02 +0000

    Why doesn't it work!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "parser": "babel-eslint",
+  parser: '@typescript-eslint/parser',  // Specifies the ESLint parser
   env: {
     browser: true,
     es6: true,
   },
-  extends: [
-    "plugin:react/recommended",
+  extends:  [
+    'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
     "airbnb",
   ],
   globals: {
--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
   // we may have multiple entry points.
   // https://webpack.js.org/concepts#entry
   entry: [
-    path.join(PATH_SOURCE, "./index.jsx"),
+    path.join(PATH_SOURCE, "./index.tsx"),
   ],
 
   // Tell Webpack where to emit the bundles it creates and how to name them.
@@ -66,7 +66,7 @@ module.exports = {
   },
 
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.ts', '.tsx'],
     mainFields: ["browser", "module", "main"],
   },
 
@@ -76,39 +76,18 @@ module.exports = {
   module: {
     rules: [
       {
-        // .js, .jsx
-        test: /\.(jsx?)$/, // Apply this rule to files ending in .js
-        exclude: /node_modules/, // Don't apply to files residing in node_modules
-        use: { // Use the following loader and options
-          loader: "babel-loader",
-          // We can pass options to both babel-loader and Babel. This option object
-          // will replace babel.config.js
-          options: {
-            plugins: ["@babel/plugin-proposal-class-properties"],
-            presets: [
-              ["@babel/preset-env", {
-                debug: false, // Output the targets/plugins used when compiling
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: [
+          {
+            options: {
+              eslintPath: require.resolve("eslint"),
 
-                // Configure how @babel/preset-env handles polyfills from core-js.
-                // https://babeljs.io/docs/en/babel-preset-env
-                useBuiltIns: "usage",
-
-                // Specify the core-js version. Must match the version in package.json
-                corejs: 3,
-
-                // Specify which environments we support/target for our project.
-                // (We have chosen to specify targets in .browserslistrc, so there
-                // is no need to do it here.)
-                // targets: "",
-              }],
-
-              // The react preset includes several plugins that are required to write
-              // a React app. For example, it transforms JSX:
-              // <div> -> React.createElement("div")
-              "@babel/preset-react",
-            ],
+            },
+            loader: require.resolve("eslint-loader"),
           },
-        },
+        ],
       },
       // css
       {
